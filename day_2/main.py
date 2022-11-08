a4f61faef65dde7baa751e6f3cfdf907bdbeb25d
bradripple <bradripple@gmail.com>
2022-11-08 11:51:21 -0700

    Completed two_digit challange

---
+++ day_2/main.py
@@ -0,0 +1,15 @@
+#DATA TYPES
+
+#String "Hello wolrd"
+
+#Integer 123 
+
+#Float 3.14
+
+#Boolean True or False
+
+num_char = len(input("what is your name?"))
+
+new_num_char = str(num_char)
+
+print("your name has " + new_num_char + " characters.")
---
+++ day_2/two_digit.py
@@ -0,0 +1,7 @@
+two_digit_number = input("Type a two digit number: ")
+
+num_1 = two_digit_number[0]
+
+num_2 = two_digit_number[1]
+
+print(int(num_1) + int(num_2))
