825acaa3c1f348bf50f1ceecc771a795c48fbabe
bradripple <bradripple@gmail.com>
2022-11-08 12:14:26 -0700

    Completed body mass

--- day_2/main.py
+++ day_2/main.py
@@ -8,8 +8,20 @@
 
 #Boolean True or False
 
-num_char = len(input("what is your name?"))
+# num_char = len(input("what is your name?"))
 
-new_num_char = str(num_char)
+# new_num_char = str(num_char)
 
-print("your name has " + new_num_char + " characters.")
+# print("your name has " + new_num_char + " characters.")
+
+# ** = power of
+
+# PEMDAS - Left to Right
+# Parentheses ()
+# Exponents **
+# Mulitplication *
+# Division /
+# Addition +
+# Substraction -
+
+print(3 * 3 + 3 / 3 - 3)
