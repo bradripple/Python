c3902fc45253d21339992a942605640d39a177bd
bradripple <bradripple@gmail.com>
2022-11-28 12:44:05 -0700

    refactored leap_year with nested logic

--- day_3/leap_year.py
+++ day_3/leap_year.py
@@ -1,5 +1,6 @@
 year = int(input("Which year do you want to check? "))
 
+# elif statement logic
 if year % 4 != 0:
     print("Not leap")
 elif year % 100 != 0:
@@ -8,3 +9,15 @@ elif year % 400 == 0:
     print("Leap")
 else: 
     print("Not leap")
+
+# nested conditional logic statement
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("Leap year.")
+#         else:
+#             print("Not leap year.")
+#     else:
+#         print("Leap year.")
+# else:
+#     print("Not leap year.")
