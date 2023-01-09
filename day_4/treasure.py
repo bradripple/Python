09e2e569da0227e84126132e5d6c2ae83e427e97
bradripple <bradripple@gmail.com>
2023-01-09 13:59:11 -0700

    Created rock_paper_scissors challenge

--- day_4/treasure.py
+++ day_4/treasure.py
@@ -13,7 +13,7 @@ vertical = int(position[1])
 # row[horizontal - 1] = 'X'
 
 
-row = map[vertical - 1][horizontal - 1] = 'X'
+map[vertical - 1][horizontal - 1] = 'X'
 
 
 print(f"{row1}\n{row2}\n{row3}")
