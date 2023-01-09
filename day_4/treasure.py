80c64596dff730c3d7931cb63089a6198d91b7e4
bradripple <bradripple@gmail.com>
2023-01-09 13:49:35 -0700

    Completed treasure challenge

--- day_4/treasure.py
+++ day_4/treasure.py
@@ -5,10 +5,11 @@ map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 
+#Create useable input split two digits into list
+horizontal = int(position[0])
+vertical = int(position[1])
 
-
-
-
-
+row = map[vertical - 1]
+row[horizontal - 1] = 'X'
 
 print(f"{row1}\n{row2}\n{row3}")
