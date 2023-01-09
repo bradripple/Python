2c6b068d4e32fa63d5f609fe424195a65dbe57a9
bradripple <bradripple@gmail.com>
2023-01-09 12:43:13 -0700

    Started roulette challenge

--- day_4/main.py
+++ day_4/main.py
@@ -1,9 +1,11 @@
-import random
+# import random
 
-random_integer = random.randint(1, 10)
-print(random_integer)
+# random_integer = random.randint(1, 10)
+# print(random_integer)
 
-random_float = random.random()
-print(random_float)
+# random_float = random.random()
+# print(random_float)
 
-random_float * 5
+# random_float * 5
+
+states = ["Delaware", "Utah", "Idaho"]
---
+++ day_4/roulette.py
@@ -0,0 +1,2 @@
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
