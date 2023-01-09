1ff62ff0ee5f7b4bcd3f5320bf728d494a73d2ef
bradripple <bradripple@gmail.com>
2023-01-09 13:05:30 -0700

    Completed roulette using choice

--- day_4/roulette.py
+++ day_4/roulette.py
@@ -4,5 +4,8 @@ names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 
 
-random_index = random.randint(0,len(names)-1)
-print(f'{names[random_index]} is going to buy the meal today!')
+# random_index = random.randint(0,len(names)-1)
+# print(f'{names[random_index]} is going to buy the meal today!')
+
+buyer = random.choice(names)
+print(f'{buyer} is going to buy the meal today!')
