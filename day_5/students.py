cbb37eee2f23d3b8011871cb58641c9858dcc3cf
bradripple <bradripple@gmail.com>
2023-01-10 13:44:46 -0700

    Solved highest score with course solution

--- day_5/students.py
+++ day_5/students.py
@@ -3,13 +3,22 @@ for n in range(0, len(student_scores)):
   student_scores[n] = int(student_scores[n])
 print(student_scores)
 
-#Solution using max and min functions
+# Solution using max and min functions
 # print("The highest score in the class is: " + str(max(student_scores)))
 # print("The lowest score in the class is: " + str(min(student_scores)))
 
+# My solution
 max = 0
 
 for score in student_scores:
     if max < score:
         max = score
 print("The highest score in the class is: " + str(max))
+
+# Course solution
+highest_score = 0
+
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+print(f"The highest score in the class is: {highest_score}")
