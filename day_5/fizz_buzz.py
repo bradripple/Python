20ed4b544a9fb642b17831ce778c630ccf4cb5d7
bradripple <bradripple@gmail.com>
2023-01-10 14:18:13 -0700

    Solved fizz buzz using course solution

--- day_5/fizz_buzz.py
+++ day_5/fizz_buzz.py
@@ -1,4 +1,4 @@
-
+# My solution
 
 for num in range(1, 101):
     if num % 3 == 0 and num % 5 == 0:
@@ -9,3 +9,16 @@ for num in range(1, 101):
         print("Buzz")
     else:
         print(num)
+
+
+# Course solution - same as mine
+
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else: 
+        print(number)
