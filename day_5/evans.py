875d7e24e1a135a658e7ce3bbf6e400d591ea971
bradripple <bradripple@gmail.com>
2023-01-10 14:01:00 -0700

    Solved adding evans with two course solutions

--- day_5/evans.py
+++ day_5/evans.py
@@ -5,3 +5,19 @@ count = 0
 for number in range(0, 101, 2):
     count += number
 print(count)
+
+# Course solution 1
+total = 0
+
+for number in range(2, 101, 2):
+    total += number
+print(total)
+
+
+# Course solution 2
+total2 = 0
+
+for number in range(1, 101):    
+    if number % 2 == 0:
+        total2 += number
+print(total2)
